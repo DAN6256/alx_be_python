ca6580d873b5f82824ab48c688efdebdc20bcd47
Daniel Tunyinko <daniel.tunyinko@gmail.com>
2025-07-07 18:19:19 +0000

    Corrects errors with variable names

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
-monthly_income = float(input("Enter your monthly income:"))
-total_monthly_expenses = float(input("Enter your total monthly expenses:"))
-monthly_savings = monthly_income - total_monthly_expenses
+monthly_incomes = float(input("Enter your monthly income:"))
+monthly_expenses = float(input("Enter your total monthly expenses:"))
+monthly_savings = monthly_incomes - monthly_expenses
 
 rate = 0.05
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * rate)
