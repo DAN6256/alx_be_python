2ee521c5a2f7881e3bd3b38fdbb4c5e5c165b8f4
Daniel Tunyinko <daniel.tunyinko@gmail.com>
2025-07-14 14:27:41 +0000

    Adds missing parts of reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -7,8 +7,8 @@ reminder = f"'{task}' is a {priority} task"
 match priority:
     case "high":
         if time_bound == "yes":
-            reminder = f"'{task}' is a high priority task that requires immediate attention today!"
+            reminder = f"Reminder: '{task}' is a high priority task that requires immediate attention today!"
             print(reminder)
     case "low":
-        reminder = f"'{task}' is a low priority task.Consider completing it when you have free time!"
+        reminder = f"Note: '{task}' is a low priority task.Consider completing it when you have free time!"
         print(reminder)
