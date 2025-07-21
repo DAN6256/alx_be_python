aadac1994820055879a2673afa1cda4bc74b39c7
Daniel Tunyinko <daniel.tunyinko@gmail.com>
2025-07-21 12:53:35 +0000

    Correct import

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,9 +1,10 @@
-import datetime
+from datetime import datetime
 
 def display_current_datetime():
-    current_date = datetime.datetime.now()
+    current_date = datetime.now()
     formatted = current_date.strftime("%Y-%m-%d %H:%M:%S")
     print(f"Current date and time: {formatted}")
+
 def calculate_future_date():
     current_date = datetime.date.today()
     num_days = int(input("Enter number of days: "))
