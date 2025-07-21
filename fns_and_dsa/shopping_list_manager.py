6075a52c755ef852b15975cfe6ed6911de62b5a1
Daniel Tunyinko <daniel.tunyinko@gmail.com>
2025-07-21 11:32:05 +0000

    correct message in input

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -12,7 +12,7 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            item = input("Enter name of item to add: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
         elif choice == '2':
             name = input("Enter name of item to remove: ")
