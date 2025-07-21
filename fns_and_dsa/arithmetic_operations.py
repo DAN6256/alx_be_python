b7eca59a2504c66aa6643b2ff8a072b23090f411
Daniel Tunyinko <daniel.tunyinko@gmail.com>
2025-07-21 11:18:21 +0000

    Use elif for divide by 0

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -12,8 +12,9 @@ def perform_operation(num1, num2, operation):
         case "divide":
             if num2 == 0:
                 return "Cannot divide by 0"
-            result = num1/num2
-            return result
+            elif num2 !=0:
+                result = num1/num2
+                return result
         case _ :
             return f"No such operation {operation} exit"
         
