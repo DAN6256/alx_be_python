de5cf78eee758c12f7e096021263fd3ffb0ad6d7
Daniel Tunyinko <daniel.tunyinko@gmail.com>
2025-07-21 12:08:21 +0000

    First attempt on dates

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,14 @@
+import datetime
+
+def display_current_datetime():
+    current_date = datetime.datetime.now()
+    formatted = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Current date and time: {formatted}")
+def calculate_future_date():
+    current_date = datetime.date.today()
+    num_days = int(input("Enter number of days: "))
+    future_date = current_date + datetime.timedelta(days=num_days)
+    formatted_date = future_date.strftime("%Y-%m-%d")
+    print(f"Future date: {formatted_date}")
+display_current_datetime()
+calculate_future_date()
