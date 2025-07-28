7310b4cc30c8951c20fff3cf4bad0ccc2684ba11
Daniel Tunyinko <daniel.tunyinko@gmail.com>
2025-07-28 14:12:15 +0000

    Change instance from calculator to calc

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -4,23 +4,23 @@ from simple_calculator import SimpleCalculator
 class TestSimpleCalculator(unittest.TestCase):
 
     def setUp(self):
-        self.calculator = SimpleCalculator()
+        self.calc = SimpleCalculator()
 
     def test_add(self):
-        self.assertEqual(self.calculator.add(2, 3), 5)
-        self.assertEqual(self.calculator.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
 
     def test_subtract(self):
-        self.assertEqual(self.calculator.subtract(5, 3), 2)
-        self.assertEqual(self.calculator.subtract(3, 5), -2)
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(3, 5), -2)
 
     def test_multiply(self):
-        self.assertEqual(self.calculator.multiply(2, 3), 6)
-        self.assertEqual(self.calculator.multiply(-1, 1), -1)
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(-1, 1), -1)
 
     def test_divide(self):
-        self.assertEqual(self.calculator.divide(6, 3), 2)
-        self.assertEqual(self.calculator.divide(5, 0), None)
+        self.assertEqual(self.calc.divide(6, 3), 2)
+        self.assertEqual(self.calc.divide(5, 0), None)
 
 if __name__ == "__main__":
     unittest.main()
