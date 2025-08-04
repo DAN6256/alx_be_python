a5c469fa1eaf92b80c292d385e8c68cc12ab31d4
Daniel Tunyinko <daniel.tunyinko@gmail.com>
2025-08-04 21:39:09 +0000

    Add string representation for classes

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,16 +2,27 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
+
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}"
+
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
+
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+
+
 
 class Library:
     def __init__(self):
@@ -22,12 +33,7 @@ class Library:
     
     def list_books(self):
         for book in self.books:
-            if book.__class__.__name__ == "EBook":
-                print(f"{book.__class__.__name__}: {book.title} by {book.author}, File Size: {book.file_size}")
-            elif book.__class__.__name__ == "PrintBook":
-                print(f"{book.__class__.__name__}: {book.title} by {book.author}, Page Count: {book.page_count}")
-            elif book.__class__.__name__ == "Book":
-                print(f"{book.__class__.__name__}: {book.title} by {book.author}")
+            print(book)
 
 
         
